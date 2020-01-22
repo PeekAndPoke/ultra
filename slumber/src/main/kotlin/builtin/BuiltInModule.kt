7881394bff4f4d66a9a747cbd034c3cd4d8a01b7
PeekAndPoke <karsten.john.gerber@googlemail.com>
2020-01-22 11:53:22 +0100

    slumber: Fix for slumbering iterable without type information

--- slumber/src/main/kotlin/builtin/BuiltInModule.kt
+++ slumber/src/main/kotlin/builtin/BuiltInModule.kt
@@ -1,6 +1,6 @@
 package de.peekandpoke.ultra.slumber.builtin
 
-import de.peekandpoke.ultra.common.kType
+import de.peekandpoke.ultra.common.TypeRef
 import de.peekandpoke.ultra.slumber.Awaker
 import de.peekandpoke.ultra.slumber.SlumberModule
 import de.peekandpoke.ultra.slumber.Slumberer
@@ -26,45 +26,67 @@ object BuiltInModule : SlumberModule {
 
             return when {
                 // Null or Nothing
-                cls in listOf(Nothing::class, Unit::class) -> NullCodec
+                cls in listOf(Nothing::class, Unit::class) ->
+                    NullCodec
 
                 // Any type
-                cls in listOf(Any::class, Serializable::class) -> type.wrapIfNonNull(AnyAwaker)
+                cls in listOf(Any::class, Serializable::class) ->
+                    type.wrapIfNonNull(AnyAwaker)
 
                 // Primitive types
-                cls == Number::class -> type.wrapIfNonNull(NumberAwaker)
-                cls == Boolean::class -> type.wrapIfNonNull(BooleanAwaker)
-                cls == Byte::class -> type.wrapIfNonNull(ByteAwaker)
-                cls == Char::class -> type.wrapIfNonNull(CharAwaker)
-                cls == Double::class -> type.wrapIfNonNull(DoubleAwaker)
-                cls == Float::class -> type.wrapIfNonNull(FloatAwaker)
-                cls == Int::class -> type.wrapIfNonNull(IntAwaker)
-                cls == Long::class -> type.wrapIfNonNull(LongAwaker)
-                cls == Short::class -> type.wrapIfNonNull(ShortAwaker)
+                cls == Number::class ->
+                    type.wrapIfNonNull(NumberAwaker)
+                cls == Boolean::class ->
+                    type.wrapIfNonNull(BooleanAwaker)
+                cls == Byte::class ->
+                    type.wrapIfNonNull(ByteAwaker)
+                cls == Char::class ->
+                    type.wrapIfNonNull(CharAwaker)
+                cls == Double::class ->
+                    type.wrapIfNonNull(DoubleAwaker)
+                cls == Float::class ->
+                    type.wrapIfNonNull(FloatAwaker)
+                cls == Int::class ->
+                    type.wrapIfNonNull(IntAwaker)
+                cls == Long::class ->
+                    type.wrapIfNonNull(LongAwaker)
+                cls == Short::class ->
+                    type.wrapIfNonNull(ShortAwaker)
 
                 // Strings
                 cls == String::class -> type.wrapIfNonNull(StringAwaker)
 
                 // Lists
-                cls == Iterable::class || cls == List::class -> type.wrapIfNonNull(CollectionAwaker.forList(type))
-                cls == MutableList::class -> type.wrapIfNonNull(CollectionAwaker.forMutableList(type))
-                cls == Set::class -> type.wrapIfNonNull(CollectionAwaker.forSet(type))
-                cls == MutableSet::class -> type.wrapIfNonNull(CollectionAwaker.forMutableSet(type))
+                cls == Iterable::class || cls == List::class ->
+                    type.wrapIfNonNull(CollectionAwaker.forList(type))
+                cls == MutableList::class ->
+                    type.wrapIfNonNull(CollectionAwaker.forMutableList(type))
+                cls == Set::class ->
+                    type.wrapIfNonNull(CollectionAwaker.forSet(type))
+                cls == MutableSet::class ->
+                    type.wrapIfNonNull(CollectionAwaker.forMutableSet(type))
 
                 // Maps
-                cls == Map::class -> type.wrapIfNonNull(MapAwaker.forMap(type))
-                cls == MutableMap::class -> type.wrapIfNonNull(MapAwaker.forMutableMap(type))
+                cls == Map::class ->
+                    type.wrapIfNonNull(MapAwaker.forMap(type))
+                cls == MutableMap::class ->
+                    type.wrapIfNonNull(MapAwaker.forMutableMap(type))
 
                 // Enum
-                cls.java.isEnum -> type.wrapIfNonNull(EnumCodec(type) as Awaker)
+                cls.java.isEnum ->
+                    type.wrapIfNonNull(EnumCodec(type) as Awaker)
 
                 // Polymorphic classes
-                Polymorphic.supports(cls) -> type.wrapIfNonNull(Polymorphic.createAwaker(cls))
+                Polymorphic.supports(cls) ->
+                    type.wrapIfNonNull(Polymorphic.createAwaker(cls))
+
                 // Data classes
-                cls.isData -> type.wrapIfNonNull(DataClassCodec(type) as Awaker)
+                cls.isData ->
+                    type.wrapIfNonNull(DataClassCodec(type) as Awaker)
+
                 // No param ctor
-                cls.primaryConstructor != null && cls.primaryConstructor!!.parameters.isEmpty()
-                -> type.wrapIfNonNull(DataClassCodec(type) as Awaker)
+                cls.primaryConstructor != null && cls.primaryConstructor!!.parameters.isEmpty() ->
+                    type.wrapIfNonNull(DataClassCodec(type) as Awaker)
 
                 // Type cannot be handled by this module
                 else -> null
@@ -86,46 +108,65 @@ object BuiltInModule : SlumberModule {
                 cls in listOf(Nothing::class, Unit::class) -> NullCodec
 
                 // Any, Object or Serializable type
-                cls in listOf(Any::class, Serializable::class) -> type.wrapIfNonNull(AnySlumberer)
+                cls in listOf(Any::class, Serializable::class) ->
+                    type.wrapIfNonNull(AnySlumberer)
 
                 // Primitive types
-                cls == Number::class -> type.wrapIfNonNull(NumberSlumberer)
-                cls == Boolean::class -> type.wrapIfNonNull(BooleanSlumberer)
-                cls == Byte::class -> type.wrapIfNonNull(ByteSlumberer)
-                cls == Char::class -> type.wrapIfNonNull(CharSlumberer)
-                cls == Double::class -> type.wrapIfNonNull(DoubleSlumberer)
-                cls == Float::class -> type.wrapIfNonNull(FloatSlumberer)
-                cls == Int::class -> type.wrapIfNonNull(IntSlumberer)
-                cls == Long::class -> type.wrapIfNonNull(LongSlumberer)
-                cls == Short::class -> type.wrapIfNonNull(ShortSlumberer)
+                cls == Number::class ->
+                    type.wrapIfNonNull(NumberSlumberer)
+                cls == Boolean::class ->
+                    type.wrapIfNonNull(BooleanSlumberer)
+                cls == Byte::class ->
+                    type.wrapIfNonNull(ByteSlumberer)
+                cls == Char::class ->
+                    type.wrapIfNonNull(CharSlumberer)
+                cls == Double::class ->
+                    type.wrapIfNonNull(DoubleSlumberer)
+                cls == Float::class ->
+                    type.wrapIfNonNull(FloatSlumberer)
+                cls == Int::class ->
+                    type.wrapIfNonNull(IntSlumberer)
+                cls == Long::class ->
+                    type.wrapIfNonNull(LongSlumberer)
+                cls == Short::class ->
+                    type.wrapIfNonNull(ShortSlumberer)
 
                 // Strings
-                cls == String::class -> type.wrapIfNonNull(StringSlumberer)
+                cls == String::class ->
+                    type.wrapIfNonNull(StringSlumberer)
 
                 // Iterables
                 Iterable::class.java.isAssignableFrom(cls.java) ->
-                    type.wrapIfNonNull(CollectionSlumberer(args[0].type!!))
+                    type.wrapIfNonNull(
+                        CollectionSlumberer(
+                            args.getOrNull(0)?.type ?: TypeRef.Any.type
+                        )
+                    )
 
                 // Maps
                 Map::class.java.isAssignableFrom(cls.java) ->
                     type.wrapIfNonNull(
-                        when (args.size) {
-                            2 -> MapSlumberer(args[0].type!!, args[1].type!!)
-
-                            else -> MapSlumberer(kType<String>().type, kType<Any>().type)
-                        }
+                        MapSlumberer(
+                            args.getOrNull(0)?.type ?: TypeRef.String.type,
+                            args.getOrNull(1)?.type ?: TypeRef.Any.type
+                        )
                     )
 
                 // Enum
-                cls.java.isEnum -> type.wrapIfNonNull(EnumCodec(type) as Slumberer)
+                cls.java.isEnum ->
+                    type.wrapIfNonNull(EnumCodec(type) as Slumberer)
 
                 // Polymorphic classes
-                Polymorphic.supports(cls) -> type.wrapIfNonNull(Polymorphic.createSlumberer(cls))
+                Polymorphic.supports(cls) ->
+                    type.wrapIfNonNull(Polymorphic.createSlumberer(cls))
+
                 // Data classes
-                cls.isData -> type.wrapIfNonNull(DataClassCodec(type) as Slumberer)
+                cls.isData ->
+                    type.wrapIfNonNull(DataClassCodec(type) as Slumberer)
+
                 // No param ctor
-                cls.primaryConstructor != null && cls.primaryConstructor!!.parameters.isEmpty()
-                -> type.wrapIfNonNull(DataClassCodec(type) as Slumberer)
+                cls.primaryConstructor != null && cls.primaryConstructor!!.parameters.isEmpty() ->
+                    type.wrapIfNonNull(DataClassCodec(type) as Slumberer)
 
                 else -> null
             }
