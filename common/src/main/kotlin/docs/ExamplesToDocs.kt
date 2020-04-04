8d34808ad90303f632c35ee13c7089cf35a9b66f
PeekAndPoke <karsten.john.gerber@googlemail.com>
2020-04-05 00:49:41 +0200

    detekt configuration

--- common/src/main/kotlin/docs/ExamplesToDocs.kt
+++ common/src/main/kotlin/docs/ExamplesToDocs.kt
@@ -50,11 +50,11 @@ class ExamplesToDocs internal constructor(
 
         chapters.forEachIndexed { chapterIndex, chapter ->
 
-            builder.appendln("${chapterIndex}. ${chapter.name}").appendln()
+            builder.appendln("* ${chapter.name}").appendln()
 
             chapter.examples.forEachIndexed { exampleIndex, example ->
 
-                builder.appendln("    ${exampleIndex}. [${example.title}](#${example.title})")
+                builder.appendln("    * [${example.title}](#${example.title})")
             }
         }
     }
