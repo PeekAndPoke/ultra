8179dc51f0ca2f7599f553b0873dc0b29a499817
PeekAndPoke <karsten.john.gerber@googlemail.com>
2020-04-11 14:48:49 +0200

    mutator: docs

--- mutator/src/examples/introduction/SimpleExample.kt
+++ mutator/src/examples/introduction/SimpleExample.kt
@@ -38,7 +38,7 @@ class SimpleExample : SimpleExample() {
         println("The original:")
         println(angelina)
         // But we got another version with increased age.
-        println("The older version:")
+        println("The new version with modified age:")
         println(olderAngelina)
 
         // We could also change the name or both.
