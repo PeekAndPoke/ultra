c0024ba1bd2441ec0a6db555d621b0caa459352b
PeekAndPoke <karsten.john.gerber@googlemail.com>
2020-05-13 12:34:14 +0200

    security: added convenience methods to UserPermissions

--- security/src/main/kotlin/user/permissions.kt
+++ security/src/main/kotlin/user/permissions.kt
@@ -1,10 +1,57 @@
 package de.peekandpoke.ultra.security.user
 
+import de.peekandpoke.ultra.common.containsAny
+
 data class UserPermissions(
     val groups: List<String> = emptyList(),
     val roles: List<String> = emptyList(),
     val permissions: List<String> = emptyList()
 )
+
+/**
+ * Return 'true' when the given [group] is present
+ */
+fun UserPermissions.hasGroup(group: String) = this.groups.contains(group)
+
+/**
+ * Return 'true' when any of the given [groups] is present
+ */
+fun UserPermissions.hasAnyGroup(groups: List<String>) = this.groups.containsAny(groups)
+
+/**
+ * Return 'true' when all of the given [groups] are present
+ */
+fun UserPermissions.hasAllGroups(groups: List<String>) = this.groups.containsAll(groups)
+
+/**
+ * Return 'true' when the given [role] is present
+ */
+fun UserPermissions.hasRole(role: String) = this.roles.contains(role)
+
+/**
+ * Return 'true' when any of the given [roles] is present
+ */
+fun UserPermissions.hasAnyRole(roles: List<String>) = this.roles.containsAny(roles)
+
+/**
+ * Return 'true' when all of the given [roles] are present
+ */
+fun UserPermissions.hasAllRoles(roles: List<String>) = this.roles.containsAll(roles)
+
+/**
+ * Return 'true' when the given [permission] is present
+ */
+fun UserPermissions.hasPermission(permission: String) = this.permissions.contains(permission)
+
+/**
+ * Return 'true' when any of the given [permissions] is present
+ */
+fun UserPermissions.hasAnyPermission(permissions: List<String>) = this.permissions.containsAny(permissions)
+
+/**
+ * Return 'true' when all of the given [permissions] are present
+ */
+fun UserPermissions.hasAllPermissions(permissions: List<String>) = this.permissions.containsAll(permissions)
 
 interface UserPermissionsProvider {
     operator fun invoke(): UserPermissions
