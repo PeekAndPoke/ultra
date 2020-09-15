61a9d3f7c7c88fa222146b8fc7337926b507da29
PeekAndPoke <karsten.john.gerber@googlemail.com>
2020-09-15 08:41:48 +0200

    security: added to do

--- security/src/main/kotlin/index.kt
+++ security/src/main/kotlin/index.kt
@@ -10,6 +10,8 @@ import de.peekandpoke.ultra.security.user.UserPermissionsProvider
 import de.peekandpoke.ultra.security.user.UserRecordProvider
 
 data class UltraSecurityConfig(
+    // TODO: [Issue][https://github.com/PeekAndPoke/ultra/issues/4]
+    //       Add @get:JsonIgnore so the value will not be visible on logs or insights etc.
     val csrfSecret: String,
     val csrfTtlMillis: Int
 )
