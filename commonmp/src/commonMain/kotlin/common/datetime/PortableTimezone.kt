149eefd85ae1ac65f54610d42a504ddb9fd36478
PeekAndPoke <karsten.john.gerber@googlemail.com>
2021-02-23 22:33:58 +0100

    common-mp: more portable date classes

--- commonmp/src/commonMain/kotlin/common/datetime/PortableTimezone.kt
+++ commonmp/src/commonMain/kotlin/common/datetime/PortableTimezone.kt
@@ -3,4 +3,8 @@ package de.peekandpoke.ultra.common.datetime
 import kotlinx.serialization.Serializable
 
 @Serializable(with = PortableTimezoneSerializer::class)
-data class PortableTimezone(val id: String)
+data class PortableTimezone(val id: String) {
+    companion object {
+        val UTC = PortableTimezone("UTC")
+    }
+}
