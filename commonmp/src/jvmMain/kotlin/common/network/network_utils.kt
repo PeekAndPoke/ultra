12814bab78bae1b8b1879aaa36439d990334d73d
PeekAndPoke <karsten.john.gerber@googlemail.com>
2021-11-29 11:11:43 +0100

    common: NetworkUtils helper to get network fingerprint

--- commonmp/src/jvmMain/kotlin/common/network/network_utils.kt
+++ commonmp/src/jvmMain/kotlin/common/network/network_utils.kt
@@ -1,19 +1,37 @@
 package de.peekandpoke.ultra.common.network
 
+import de.peekandpoke.ultra.common.md5
 import java.net.InetAddress
+import java.net.NetworkInterface
+import java.net.SocketException
 import java.net.UnknownHostException
+import java.time.Instant
 
 object NetworkUtils {
+
     fun getHostNameOrDefault(default: String = "unknown") = try {
         InetAddress.getLocalHost()?.hostName ?: default
     } catch (e: UnknownHostException) {
+        e.printStackTrace()
         default
     }
 
-    fun getLocalhostIp() = try {
-        InetAddress.getLocalHost()?.hostAddress ?: "unknown"
-    } catch (E: UnknownHostException) {
-        "unknown"
+    fun getNetworkFingerPrint() = try {
+
+        NetworkInterface.getNetworkInterfaces().toList()
+            .filterNotNull()
+            .flatMap { iface ->
+                listOf(
+                    iface.name
+                ).plus(
+                    iface.inetAddresses.toList().mapNotNull { it.hostAddress }
+                )
+            }
+            .joinToString("")
+            .md5()
+
+    } catch (e: SocketException) {
+        Instant.now().toString().md5()
     }
 }
 
--- z-playground-jvm/src/main/kotlin/main.kt
+++ z-playground-jvm/src/main/kotlin/main.kt
@@ -1,39 +1,9 @@
 package de.peekandpoke.ultra.playground
 
-import de.peekandpoke.ultra.kontainer.kontainer
+import de.peekandpoke.ultra.common.network.NetworkUtils
 
 fun main() {
 
-    // We define a service that will be injected
-    class Counter {
-        private var count = 0
-        fun next() = ++count
-    }
-
-    // We define a service that injects another service in it's constructor.
-    // But this time this service also expects a second parameter that cannot be provided by the kontainer.
-    class MyService(private val counter: Counter, private val offset: Int) {
-        fun next() = counter.next() + offset
-    }
-
-    // We define the kontainer blueprint
-    val blueprint = kontainer {
-        // We define the service using a factory method.
-        // Injection is now only done for all parameters of the factory method.
-        singleton { counter: Counter ->
-            MyService(counter, 100)
-        }
-
-        singleton(Counter::class)
-    }
-
-    // We get the kontainer instance
-    val kontainer = blueprint.create()
-
-    // We use the service
-    val myService = kontainer.get(MyService::class)
-
-    println("Next: " + myService.next())
-    println("Next: " + myService.next())
+    println("${NetworkUtils.getHostNameOrDefault()}-${NetworkUtils.getNetworkFingerPrint()}")
 
 }
