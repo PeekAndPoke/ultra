c7dcf0d6d65f331a1b2f5fcfdc3c674f4f9c1429
PeekAndPoke <kontakt@karsten-gerber.de>
2025-02-25 12:07:08 +0100

    helpers

---
+++ common/src/commonMain/kotlin/common/reflection.kt
@@ -0,0 +1,19 @@
+package de.peekandpoke.ultra.common
+
+import kotlin.reflect.KType
+
+// TODO: test me
+val KType.isPrimitive
+    get() : Boolean = classifier in listOf(
+        Boolean::class,
+        Char::class,
+        Byte::class,
+        Short::class,
+        Int::class,
+        Long::class,
+        Float::class,
+        Double::class
+    )
+
+// TODO: test me
+val KType.isPrimitiveOrString get(): Boolean = classifier == String::class || isPrimitive
