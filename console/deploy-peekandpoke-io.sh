506e5075aaa5b1e47de49fdc335117336dfe12cc
PeekAndPoke <kontakt@karsten-gerber.de>
2026-03-25 17:02:00 +0100

    docs site script

--- console/deploy-peekandpoke-io.sh
+++ console/deploy-peekandpoke-io.sh
@@ -1,16 +1,11 @@
 #!/bin/bash
+set -euo pipefail
 
-# Get caller directory
-initial_wd=`pwd`
+cd docs-site || exit
+pnpm install
+pnpm run build
 
-echo "Switching to dir $initial_wd"
-pwd
-
-shopt -s dotglob
+cd ..
 
 echo "Uploading JS files"
-scp -r ./build/dist/js/productionExecutable/* finzo:/www/htdocs/w0057ac0/finzo/klang.finzo.de
-#scp -r ./build/kotlin-webpack/js/developmentExecutable/* finzo:/www/htdocs/w0057ac0/finzo/klang.finzo.de
-
-echo "Uploading resources"
-scp -r ./src/jsMain/resources/* finzo:/www/htdocs/w0057ac0/finzo/klang.finzo.de/
+scp -r ./docs-site/dist/* finzo:/www/htdocs/w0057ac0/peekandpoke.io
