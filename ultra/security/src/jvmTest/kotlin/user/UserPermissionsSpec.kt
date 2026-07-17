93211e15b82ffd72e190292cbc4b183fb897fdcf
PeekAndPoke <karsten.john.gerber@googlemail.com>
2026-07-18 00:44:05 +0200

    funktor auth updates - UserPermissionsSpec tests

--- ultra/security/src/jvmTest/kotlin/user/UserPermissionsSpec.kt
+++ ultra/security/src/jvmTest/kotlin/user/UserPermissionsSpec.kt
@@ -142,6 +142,11 @@ class UserPermissionsSpec : FreeSpec() {
                         subject.hasAnyOrganisation(test) shouldBe expected
                     }
                 }
+
+                // vararg overload
+                UserPermissions(org = "a").hasAnyOrganisation("a", "X") shouldBe true
+                UserPermissions(org = "a").hasAnyOrganisation("b", "X") shouldBe false
+                UserPermissions(isSuperUser = true).hasAnyOrganisation("x") shouldBe true
             }
 
             "canAccessOrg" {
@@ -158,6 +163,89 @@ class UserPermissionsSpec : FreeSpec() {
                         subject.canAccessOrg(test) shouldBe expected
                     }
                 }
+            }
+
+            "hasBranch" {
+                UserPermissions(branches = setOf("a", "b")).hasBranch("a") shouldBe true
+                UserPermissions(branches = setOf("a", "b")).hasBranch("c") shouldBe false
+                UserPermissions().hasBranch("a") shouldBe false
+                UserPermissions(isSuperUser = true).hasBranch("a") shouldBe true
+            }
+
+            "hasAnyBranch / hasAllBranches (collection + vararg)" {
+                val subject = UserPermissions(branches = setOf("a", "b"))
+
+                subject.hasAnyBranch(setOf("a", "x")) shouldBe true
+                subject.hasAnyBranch("a", "x") shouldBe true
+                subject.hasAnyBranch(setOf("x", "y")) shouldBe false
+                subject.hasAnyBranch("x", "y") shouldBe false
+                subject.hasAnyBranch(emptyList<String>()) shouldBe false
+                UserPermissions().hasAnyBranch("a") shouldBe false
+                UserPermissions(isSuperUser = true).hasAnyBranch("anything") shouldBe true
+
+                subject.hasAllBranches(setOf("a", "b")) shouldBe true
+                subject.hasAllBranches("a", "b") shouldBe true
+                subject.hasAllBranches(setOf("a", "x")) shouldBe false
+                subject.hasAllBranches("a", "x") shouldBe false
+                subject.hasAllBranches(emptyList<String>()) shouldBe true // containsAll(∅) is vacuously true
+                UserPermissions(isSuperUser = true).hasAllBranches("anything") shouldBe true
+            }
+
+            "hasAnyGroup / hasAllGroups (collection + vararg)" {
+                val subject = UserPermissions(groups = setOf("a", "b"))
+
+                subject.hasAnyGroup(setOf("a", "x")) shouldBe true
+                subject.hasAnyGroup("a", "x") shouldBe true
+                subject.hasAnyGroup(setOf("x", "y")) shouldBe false
+                subject.hasAnyGroup("x", "y") shouldBe false
+                subject.hasAnyGroup(emptyList<String>()) shouldBe false
+                UserPermissions().hasAnyGroup("a") shouldBe false
+                UserPermissions(isSuperUser = true).hasAnyGroup("anything") shouldBe true
+
+                subject.hasAllGroups(setOf("a", "b")) shouldBe true
+                subject.hasAllGroups("a", "b") shouldBe true
+                subject.hasAllGroups(setOf("a", "x")) shouldBe false
+                subject.hasAllGroups("a", "x") shouldBe false
+                subject.hasAllGroups(emptyList<String>()) shouldBe true
+                UserPermissions(isSuperUser = true).hasAllGroups("anything") shouldBe true
+            }
+
+            "hasAnyRole / hasAllRoles (collection + vararg)" {
+                val subject = UserPermissions(roles = setOf("a", "b"))
+
+                subject.hasAnyRole(setOf("a", "x")) shouldBe true
+                subject.hasAnyRole("a", "x") shouldBe true
+                subject.hasAnyRole(setOf("x", "y")) shouldBe false
+                subject.hasAnyRole("x", "y") shouldBe false
+                subject.hasAnyRole(emptyList<String>()) shouldBe false
+                UserPermissions().hasAnyRole("a") shouldBe false
+                UserPermissions(isSuperUser = true).hasAnyRole("anything") shouldBe true
+
+                subject.hasAllRoles(setOf("a", "b")) shouldBe true
+                subject.hasAllRoles("a", "b") shouldBe true
+                subject.hasAllRoles(setOf("a", "x")) shouldBe false
+                subject.hasAllRoles("a", "x") shouldBe false
+                subject.hasAllRoles(emptyList<String>()) shouldBe true
+                UserPermissions(isSuperUser = true).hasAllRoles("anything") shouldBe true
+            }
+
+            "hasAnyPermission / hasAllPermissions (collection + vararg)" {
+                val subject = UserPermissions(permissions = setOf("a", "b"))
+
+                subject.hasAnyPermission(setOf("a", "x")) shouldBe true
+                subject.hasAnyPermission("a", "x") shouldBe true
+                subject.hasAnyPermission(setOf("x", "y")) shouldBe false
+                subject.hasAnyPermission("x", "y") shouldBe false
+                subject.hasAnyPermission(emptyList<String>()) shouldBe false
+                UserPermissions().hasAnyPermission("a") shouldBe false
+                UserPermissions(isSuperUser = true).hasAnyPermission("anything") shouldBe true
+
+                subject.hasAllPermissions(setOf("a", "b")) shouldBe true
+                subject.hasAllPermissions("a", "b") shouldBe true
+                subject.hasAllPermissions(setOf("a", "x")) shouldBe false
+                subject.hasAllPermissions("a", "x") shouldBe false
+                subject.hasAllPermissions(emptyList<String>()) shouldBe true
+                UserPermissions(isSuperUser = true).hasAllPermissions("anything") shouldBe true
             }
         }
     }
